3b99baf240a8462c0b2b83efde0660fcbe347a60
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2025-03-12 09:56:34 +0200

    Add files via upload

---
+++ practicum/week 04/programmer.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <cstring>
+#pragma warning (disable : 4996)
+
+namespace Constants
+{
+	constexpr size_t MAX_NAME_LEN = 32;
+	constexpr unsigned MIN_AGE = 18;
+	constexpr unsigned MIN_SALARY = 2500;
+	constexpr unsigned MAX_SALARY = 5000;
+	constexpr size_t PROGRAMMING_LANGUAGES_COUNT = 5;
+}
+
+enum class ProgrammingLanguages
+{
+	Cpp,
+	Python,
+	Java,
+	Csharp,
+	Javascript
+};
+
+const char* getLanguageString(ProgrammingLanguages langauge)
+{
+	switch (langauge)
+	{
+	case ProgrammingLanguages::Cpp:
+		return "C++";
+	case ProgrammingLanguages::Python:
+		return "Python";
+	case ProgrammingLanguages::Java:
+		return "Java";
+	case ProgrammingLanguages::Csharp:
+		return "C#";
+	case ProgrammingLanguages::Javascript:
+		return "Javascript";
+	}
+
+	return "Unknown";
+}
+
+class Programmer
+{
+private:
+	char name[Constants::MAX_NAME_LEN + 1]{};
+	unsigned age = 0;
+	unsigned salary = 0;
+	uint8_t languages = 0;
+
+	void setName(const char* name)
+	{
+		if (!name || strlen(name) > Constants::MAX_NAME_LEN)
+		{
+			strcpy(this->name, "Unknown");
+		}
+
+		strcpy(this->name, name);
+	}
+
+	void setAge(unsigned age)
+	{
+		if (age < Constants::MIN_AGE)
+		{
+			this->age = Constants::MIN_AGE;
+			return;
+		}
+
+		this->age = age;
+	}
+
+	void printProgrammingLanguages() const
+	{
+		for (size_t i = 0; i < Constants::PROGRAMMING_LANGUAGES_COUNT; i++)
+		{
+			if (languages & (1 << i))
+			{
+				std::cout << getLanguageString((ProgrammingLanguages)i) << ' ';
+			}
+		}
+	}
+
+public:
+
+	Programmer() = default;
+
+	Programmer(const char* name, unsigned age, unsigned salary)
+	{
+		setName(name);
+		setAge(age);
+		setSalary(salary);
+	}
+
+	void setSalary(unsigned salary)
+	{
+		if (Constants::MIN_SALARY > salary || salary > Constants::MAX_SALARY)
+		{
+			this->salary = Constants::MIN_SALARY;
+			return;
+		}
+
+		this->salary = salary;
+	}
+
+	const char* getName() const
+	{
+		return name;
+	}
+
+	unsigned getAge() const
+	{
+		return age;
+	}
+
+	unsigned getSalary() const
+	{
+		return salary;
+	}
+
+	void learnProgrammingLanguage(ProgrammingLanguages language)
+	{
+		languages |= 1 << (int)language;
+	}
+
+	bool knowsProgrammingLanguage(ProgrammingLanguages language) const
+	{
+		return languages & (1 << (int)languages);
+	}
+
+	void print() const
+	{
+		std::cout << name << " | " << age << " | " << salary<< " | ";
+		printProgrammingLanguages();
+	}
+};
+
+int main()
+{
+	Programmer pr("Ivan", 40, 3000);
+
+	pr.learnProgrammingLanguage(ProgrammingLanguages::Python);
+	pr.learnProgrammingLanguage(ProgrammingLanguages::Cpp);
+
+	std::cout << pr.knowsProgrammingLanguage(ProgrammingLanguages::Java)<<std::endl;
+	pr.print();
+	return 0;
+}
---
+++ practicum/week 04/theory.md
@@ -0,0 +1,194 @@
+# Теоретични примери #
+
+1. Какво ще изведе следният код?
+
+```c++
+class Product {
+  char name[20]{};
+  double price = 0;
+};
+
+int main() {
+  Product product;
+  std::cout<<product.price;
+  return 0;
+}
+```
+
+2. Кои от следните извиквания на функции са възможни?
+
+```c++
+class A {
+public:
+    void f() {}
+    void f() const {}
+    void g() const {
+        f();
+    }
+    void t() {}
+};
+
+void testFunc1(const A* ptr) {
+    if (!ptr) {
+        return;
+    }
+    ptr->f();
+}
+
+void testFunc2(const A& ref){
+    ref.t();
+}
+
+int main(){
+    A obj;
+    obj.f();
+    testFunc1(&obj);
+    obj.t();
+    testFunc2(obj);
+    return 0;
+}
+```
+
+3. Какво ще се изведе на стандартния изход след изпълнение на следния код?
+```c++
+#include <iostream>
+
+class X {
+	char ch = 'a';
+
+public:
+	X() {
+		std::cout << "X()" << std::endl;
+	}
+
+	X(char ch) : ch(ch) {
+		std::cout << "X(char)" << std::endl;
+	}
+
+	~X() {
+		std::cout << "~X()" << std::endl;
+	}
+};
+
+class Y{	
+public:
+
+	Y() {
+		std::cout << "Y()" << std::endl;
+	}
+
+	~Y() {
+		std::cout << "~Y()" << std::endl;
+	}
+};
+int main() {
+	Y* ptr = new Y[2]{};
+	X obj = 'a';
+	Y arr[3]{};
+
+	delete[] ptr;
+	return 0;
+}
+```
+
+4. Какво ще се изведе на стандартния изход след изпълнение на следния код?
+ ```c++
+class C {
+	double d;
+public:
+
+	C() {
+		d = 3.14;
+		std::cout << "C()" << std::endl;
+	}
+
+	C(double d) {
+		this->d = d;
+		std::cout << "C(double d)" << std::endl;
+	}
+
+	~C() {
+		std::cout << "~C()" << std::endl;
+	}
+};
+
+void f(C obj) {
+	std::cout << "f(C obj)" << std::endl;
+}
+
+int main() {
+	C obj;
+	f(5.12);
+
+	return 0;
+}
+```  
+5. Какво ще се отпечата?
+
+```c++
+#include <iostream>
+
+struct C {
+	double d;
+
+	C(double d) {
+    		this->d = d;
+    		std::cout << "C(double d)" << std::endl;
+	}
+
+	~C() {
+    		std::cout << "~C()" << std::endl;
+	}
+};
+
+
+int main() {
+	C arr[2];
+	C* ptr = new C[1];
+
+	delete[] ptr;
+}
+```
+
+6. Какво ще се изведе на стандартния изход след изпълнение на следния код?
+```c++
+class A {
+public:
+	A() {
+		std::cout << "A()" << std::endl;
+	}
+
+	~A() {
+		std::cout << "~A()" << std::endl;
+	}
+};
+
+class B {
+	A a;
+public:
+	B() {
+		std::cout << "B()" << std::endl;
+	}
+	~B() { 
+		std::cout << "~B()" << std::endl;
+	}
+};
+
+class C {
+	A a;
+	B b;
+public:
+	C() : b(), a() {
+		std::cout << "C()" << std::endl;
+	}
+	~C() {
+		std::cout << "~C()" << std::endl;
+	}
+};
+
+int main() {
+	C arr1[2];
+	C* ptr = new C();
+	return 0;
+}
+```
